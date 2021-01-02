d4cbee3cd03534c154df81a7347406e149df9e02
Aaron Contreras <aaronlcaq@gmail.com>
2021-01-02 16:21:15 -0500

    Complete forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,15 +6,25 @@ class UsersController < ApplicationController
   end
 
   def create
-    #@user = User.new(username: params[:username],
-                     #password: params[:password],
-                     #email: params[:email])
     @user = User.new(user_params)
     if @user.save
       redirect_to new_user_path
     else
       render :new
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to new_user_path
+    else
+     render :edit
+    end 
   end
 
   private
