26e733e056f5ca41654a21ab70854e67dc235828
Aaron Contreras <aaronlcaq@gmail.com>
2021-01-02 15:14:43 -0500

    Change form to use form_for helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new 
+  end
 
   def create
-    @user = User.new(username: params[:username],
-                     password: params[:password],
-                     email: params[:email])
+    #@user = User.new(username: params[:username],
+                     #password: params[:password],
+                     #email: params[:email])
+    @user = User.new(user_params)
     if @user.save
       redirect_to new_user_path
     else
